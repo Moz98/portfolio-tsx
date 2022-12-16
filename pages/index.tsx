c5283b8e7de4689ce507b7f01b6d7f497c93d753
Moz98 <marcosoliveirazacarias@gmail.com>
2022-12-16 20:01:48 -0300

    Initial projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import About from "../Components/About";
 import Experience from "../Components/Experience";
 import Header from "../Components/Header";
 import Hero from "../Components/Hero";
+import Projects from "../Components/Projects";
 import Skills from "../Components/Skills";
 
 export default function Home() {
@@ -9,21 +10,20 @@ export default function Home() {
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header />
 
-      {/* Hero */}
       <section id="hero" className="snap-center">
         <Hero />
       </section>
-      {/* About */}
       <section id="about" className="snap-start">
         <About />
       </section>
-      {/* Projects */}
+
+      <section id="projects" className=" snap-start">
+        <Projects />
+      </section>
       <section id="experience" className="snap-center">
         <Experience />
       </section>
-      {/* Experience */}
 
-      {/* Skills */}
       <section id="skills" className="snap-start">
         <Skills />
       </section>
